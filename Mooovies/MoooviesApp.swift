86795d70a257aa87f19128ff5a6d6c7bdecadee3
Ayberk Mogol <ayberk.m@yandex.com>
2022-05-23 16:18:09 +0300

    Initial Commit

---
+++ Mooovies/MoooviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoooviesApp.swift
+//  Mooovies
+//
+//  Created by Ayberk M on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoooviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
